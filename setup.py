81c3fa089e5727216a20e9d1eedfb989421331eb
LaurentMox <laurent@revolunet.com>
2013-05-27 11:30:32 +0200

    Missing dependency

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ setup(
     url='http://github.com/revolunet/pdf-factory',
     py_modules=['pdfFactory'],
     scripts=['pdfFactory.py'],
+    dependency_links=['https://github.com/revolunet/pypdftk/archive/pypdftk-0.2.tar.gz#egg=pypdftk-0.2'],
+    requires=['pypdftk (>=0.2)'],
     classifiers=['Development Status :: 4 - Beta',
                  'Environment :: Web Environment',
                  'Intended Audience :: Developers',
