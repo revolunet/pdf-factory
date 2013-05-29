671fecf6d3414240817968857731cfcb4fd797c5
Julien Bouquillon <julien@bouquillon.com>
2013-05-29 11:31:47 +0200

    docs+style

--- pdfFactory.py
+++ pdfFactory.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- encoding: UTF-8 -*-
-"""Parse a JSON to generate several PDF then merge them."""
+
+"""
+pdf-factory
+~~~~~~~~~~~
+
+Parse a JSON to generate several PDFs then merge them in a single file.
+"""
 
 import sys
 import os
@@ -26,50 +32,71 @@ __status__ = "Development"
 
 log = logging.getLogger()
 
+
 ####
 # Module configuration
+# Full path to WKHTMLTOPDF binary
 WKHTMLTOPDF = 'wkhtmltopdf-i386'
+# Path where the final PDF files are stored
 BASE_OUTPUT_DIR = "./outputs/"
+# WKHTMLTOPDF conversion timeout 
 TIMEOUT = 2400
 ###
 
 
 def usage():
-    """usage de la ligne de commande"""
+    ''' print CLI usage '''
     print "usage:\t", sys.argv[0], "foo.json"
     print "\t", sys.argv[0], "http://www.foo.bar/document.json"
 
 
-def clean_failure(tmp_dir, callback=None):
-    successCallback(False, callback)
+def clean_failure(tmp_dir, callback_url=None):
+    ''' clean tmp folder and call callback '''
+    if callback_url:
+        success_callback(False, callback_url)
     clean_tmp(tmp_dir)
     sys.exit()
 
 
 def clean_tmp(folder):
-    """supprime les fichiers et dossiers temporaires"""
+    ''' clean the temp directory '''
     log.info("Deleting temporary folder: \033[34m'%s'\033[m...", folder)
     shutil.rmtree(folder)
 
 
-def successCallback(success, callback):
-    if callback is not None:
-        if success:
-            payload = '{"success":true}'
-        else:
-            payload = '{"success":false}'
+def check_output_folder(root, path, create_folder=False):
+    ''' raise exception if path is not inside root '''
+    if not os.path.abspath(path).startswith(os.path.abspath(root)):
+        raise Exception('incorrect path:%s', path)
+    if create_folder:
+        outdir = os.path.dirname(path)
+        if not os.path.lexists(outdir):
+            os.makedirs(outdir)
+
+
+def make_tmp_file(tmp_dir, suffix='.pdf'):
+    ''' shortcut to create a new PDF file '''
+    handle, filename = tempfile.mkstemp(dir=tmp_dir, suffix=suffix)
+    os.close(handle)
+    return filename
+
+
+def success_callback(success, callback_url):
+    ''' emit a POST request to the callback_url with the generation result '''
+    if callback_url is not None:
+        payload = '{"success":%s}' % str(success).lower()
         try:
             log.info("\033[33mInforming callback\033[m")
-            requests.post(callback, data=payload, timeout=2)
+            requests.post(callback_url, data=payload, timeout=5)
         except:
             log.error("\033[33mCan't post to callback !\033[m\n%s", traceback.format_exc())
 
 
-def call_wkhtmltopdf(item, tmp_dir):
-    uri = item['uri']
-    pdf_file, pdf_filename = tempfile.mkstemp(dir=tmp_dir, suffix=".pdf")
+def call_wkhtmltopdf(uri, tmp_dir, options=[]):
+    ''' call WKHTMLTOPDF binary with a timeout and return generated file path '''
 
-    print "pdf_file: %s" % pdf_filename
+    pdf_filename = make_tmp_file(tmp_dir)
+
     log.info("\033[33mCalling WKHTMLTOPDF on '%s'\033[m", uri)
 
     wk_options = [
@@ -79,12 +106,12 @@ def call_wkhtmltopdf(item, tmp_dir):
         '--print-media-type',
         '--use-xserver',
     ]
-    if "options" in item:
-        for option in item["options"]:
-            wk_options.append(option)
+    if options:
+        wk_options += options
+
     wk_options.append(uri)
     wk_options.append(pdf_filename)
-    print "options: %s" % wk_options
+
     p = subprocess.Popen(wk_options)
     timeout = TIMEOUT
     while p.poll() is None:
@@ -98,22 +125,29 @@ def call_wkhtmltopdf(item, tmp_dir):
     return pdf_filename
 
 
-def processItem(item, tmp_dir):
+def process_item(item, tmp_dir):
+    '''Process a single item from the JSON config
+          - download remote resources
+          - convert HTML to PDF
+          - copy local resources
+          - use supplied data to fill the PDF form fields
+    '''
     log.debug("Processing item: \033[34m%s\033[m", item)
 
     uri = item['uri']
     fill_forms = True
-    doFile = True
-    if os.path.lexists(item.get('output', '')):
-        if not item.get('overwrite', True):
-            pdf_file, pdf_filename = tempfile.mkstemp(dir=tmp_dir, suffix=".pdf")
-            os.close(pdf_file)
+    process_file = True
+    if 'output' in item and os.path.lexists(item['output']):
+        # if the file has already been generated
+        if item.get('overwrite', True):
+            os.remove(item['output'])
+        else:
+            # we reuse the existing file
+            pdf_filename = make_tmp_file(tmp_dir)
             log.info("Copying existing file \033[33m'%s'\033[m to temporary \033[33m'%s'\033[m...", item['output'], pdf_filename)
             shutil.copy2(item['output'], pdf_filename)
-            doFile = False
-        else:
-            os.remove(item['output'])
-    if doFile:
+            process_file = False
+    if process_file:
         if uri.startswith(('http://', 'https://')):
             f = requests.head(uri)
             ftype = f.headers['content-type']
@@ -122,16 +156,17 @@ def processItem(item, tmp_dir):
                 # Start script recursively (ToDo)
                 print "JSON !"
             elif ftype.startswith('application/pdf'):
-                f = requests.get(uri)
-                pdf_file, pdf_filename = tempfile.mkstemp(dir=tmp_dir, suffix=".pdf")
+                # download the PDF
+                download = requests.get(uri)
+                pdf_filename = make_tmp_file(tmp_dir)
                 log.info("\033[33mSaving pdf from network to local '%s'\033[m", pdf_filename)
                 # Write PDF
-                os.write(pdf_file, f.content)
-                os.close(pdf_file)
+                with open(pdf_filename, 'wb') as f:
+                    f.write(download.content)
             elif ftype.startswith('text/html'):
-                # Make PDF form URL
+                # Make PDF from URL
                 fill_forms = False
-                pdf_filename = call_wkhtmltopdf(item, tmp_dir)
+                pdf_filename = call_wkhtmltopdf(item['uri'], tmp_dir, item.get('options'))
         else:
             # Use the 'mimetype' system command to determine filetype
             # -b is for brief response, -M is for Magic Only.
@@ -142,29 +177,26 @@ def processItem(item, tmp_dir):
                 # Start script recursively (ToDo)
                 print "JSON !"
             elif ftype == 'application/pdf':
-                pdf_file, pdf_filename = tempfile.mkstemp(dir=tmp_dir, suffix=".pdf")
-                os.close(pdf_file)
+                pdf_filename = make_tmp_file(tmp_dir)
                 log.info("Copying \033[33m'%s'\033[m to \033[33m'%s'\033[m...", uri, pdf_filename)
                 shutil.copy2(uri, pdf_filename)
             else:
                 raise Exception('Unsupported file type', ftype)
 
         if 'output' in item:
-            output = os.path.abspath(os.path.join(BASE_OUTPUT_DIR, os.path.normpath(item['output']).replace("..", "")))
-            outdir = os.path.dirname(output)
-            if not os.path.lexists(outdir):
-                os.makedirs(outdir)
+            output = os.path.join(BASE_OUTPUT_DIR, item['output'])
+            check_output_folder(BASE_OUTPUT_DIR, output, create_folder=True)
             shutil.copy2(pdf_filename, output)
 
-    # Use data (item specific as well as global) to fill PDF
+    # Use data (item specific as well as global) to fill the PDF forms
     if fill_forms and item.get('data'):
         log.info("Filling \033[33m'%s'\033[m with data...", pdf_filename)
-        filled_file, filled_filename = tempfile.mkstemp(dir=tmp_dir, suffix=".pdf")
-        os.close(filled_file)
+        filled_filename = make_tmp_file(tmp_dir)
         try:
             pypdftk.fill_form(pdf_filename, item['data'], filled_filename)
         except:
-            os.remove(filled_filename)
+            if os.path.lexists(filled_filename):
+                os.remove(filled_filename)
             raise
         pdf_filename = filled_filename
 
@@ -172,41 +204,40 @@ def processItem(item, tmp_dir):
 
 
 def process(config):
+    ''' parse the JSON configuration then process each item independently '''
     tmp_dir = tempfile.mkdtemp(prefix="pdfFactory-")
-    callback = config.get('callback', None)
+    callback_url = config.get('callback', None)
 
     if not os.path.lexists(config['output']) or config.get('overwrite', True):
         try:
             merge_list = []
             for item in config['items']:
+                item_data = {}
                 if 'data' in config:
-                    item_data = config['data'].copy()
-                else:
-                    item_data = []
+                    item_data.update(config['data'])
                 if 'data' in item:
                     item_data.update(item['data'])
                 item['data'] = item_data
-                merge_list.append(processItem(item, tmp_dir))
+                merge_list.append(process_item(item, tmp_dir))
         except KeyError as e:
             log.error("\033[31mError when parsing JSON file, some important values are missing !\033[m")
             log.error("Missing: \033[33m%s\033[m value.", e)
-            clean_failure(tmp_dir, callback)
+            clean_failure(tmp_dir, callback_url)
         except:
             log.error("\033[31mCannot proceed, got an unknown error:\033[m\n %s\n", traceback.format_exc())
-            clean_failure(tmp_dir, callback)
+            clean_failure(tmp_dir, callback_url)
 
-        # Merge PDF
+        # Merge resulting PDF
         log.debug("Merging pdf: \033[32m%s\033[m...", str(merge_list))
+
         # So A/foo/../B don't become A/
-        output = os.path.abspath(os.path.join(BASE_OUTPUT_DIR, os.path.normpath(config['output']).replace("..", "")))
-        outdir = os.path.dirname(output)
-        if not os.path.lexists(outdir):
-            os.makedirs(outdir)
+        output = os.path.join(BASE_OUTPUT_DIR, config['output'])
+        check_output_folder(BASE_OUTPUT_DIR, output, create_folder=True)
         pypdftk.concat(merge_list, output)
     else:
         log.info("Document \033[33m'%s'\033[m already exists and do not need re-generation.", config['output'])
 
-    successCallback(True, callback)
+    success_callback(True, callback_url)
     clean_tmp(tmp_dir)
 
 
@@ -214,14 +245,6 @@ def process(config):
 # Entry point
 ###
 if __name__ == '__main__':
-    # Logging configuration
-    #file handler
-    # logging.basicConfig(
-    #     filename='pdfFactory.log',
-    #     level=logging.DEBUG,
-    #     format='%(asctime)s %(levelname)s - %(message)s',
-    #     datefmt='%d/%m/%Y %H:%M:%S',
-    # )
     log.addHandler(logging.StreamHandler())
     if ("--help" in sys.argv) or ("-h" in sys.argv) or (len(sys.argv) != 2):
         log.debug('\033[33mStarted with no arg or help\033[m')
